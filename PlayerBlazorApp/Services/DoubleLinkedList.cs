b0930243562af16c63eaffe1eb9b070adacd5865
Kximm <164215607+Kximm@users.noreply.github.com>
2024-04-09 20:02:47 -0600

    eliminacion de nodo Agregar al inicio y al final

--- PlayerBlazorApp/Services/DoubleLinkedList.cs
+++ PlayerBlazorApp/Services/DoubleLinkedList.cs
@@ -4,10 +4,10 @@ namespace PlayerBlazorApp.Services
 {
     public class DoubleLinkedList
     {
-        public Nodo?  PrimerNodo { get; set; }
-        public Nodo?  UltimoNodo { get; set; }
+        public Nodo? PrimerNodo { get; set; }
+        public Nodo? UltimoNodo { get; set; }
 
-        public Nodo? NodoActual  { get; set; }
+        public Nodo? NodoActual { get; set; }
         public DoubleLinkedList()
         {
             PrimerNodo = null;
@@ -68,6 +68,54 @@ namespace PlayerBlazorApp.Services
         {
             NodoActual = NodoActual.LigaAnterior ?? PrimerNodo;
             return NodoActual;
+        }
+        public string EliminarNodoAlInicio ()
+        {
+            if (IsEmpty)
+            {
+                return "La lista está vacía. No se puede eliminar Video.";
+            }
+            else if (PrimerNodo == UltimoNodo)
+            {
+
+                PrimerNodo = null;
+                UltimoNodo = null;
+            }
+            else
+            {
+
+                PrimerNodo = PrimerNodo.LigaSiguiente;
+                PrimerNodo.LigaAnterior = null;
+            }
+
+
+            NodoActual = PrimerNodo;
+
+            return "Nodo eliminado del Inicio...";
+        }
+        public string EliminarNodoAlFinal()
+        {
+            if (IsEmpty)
+            {
+                return "La lista está vacía. No se puede eliminar Video.";
+            }
+            else if (PrimerNodo == UltimoNodo)
+            {
+                
+                PrimerNodo = null;
+                UltimoNodo = null;
+            }
+            else
+            {
+               
+                UltimoNodo = UltimoNodo.LigaAnterior;
+                UltimoNodo.LigaSiguiente = null;
+            }
+
+            
+            NodoActual = UltimoNodo;
+
+            return "Nodo eliminado del final...";
         }
 
 
