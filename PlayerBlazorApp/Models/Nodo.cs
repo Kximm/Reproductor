d186c8e07b1d0b67864ce670a11a8494a172aeb0
Kximm <164215607+Kximm@users.noreply.github.com>
2024-04-09 19:19:25 -0600

    Agregar archivos de proyecto.

---
+++ PlayerBlazorApp/Models/Nodo.cs
@@ -0,0 +1,18 @@
+﻿namespace PlayerBlazorApp.Models
+{
+    public class Nodo
+    {
+        public Nodo? LigaAnterior { get; set; }
+
+        public string Informacion { get; set; }
+
+        public Nodo? LigaSiguiente { get; set; }
+
+        public Nodo(string informacion)
+        {
+            LigaAnterior = null;
+            Informacion = informacion;
+            LigaSiguiente = null;
+        }
+    }
+}
---
+++ PlayerBlazorApp/Services/DoubleLinkedList.cs
@@ -0,0 +1,75 @@
+﻿using PlayerBlazorApp.Models;
+
+namespace PlayerBlazorApp.Services
+{
+    public class DoubleLinkedList
+    {
+        public Nodo?  PrimerNodo { get; set; }
+        public Nodo?  UltimoNodo { get; set; }
+
+        public Nodo? NodoActual  { get; set; }
+        public DoubleLinkedList()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+            NodoActual = null;
+        }
+
+        public bool IsEmpty => PrimerNodo == null;
+
+        public string AgregarNodoAlInicio(Nodo nuevoNodo)
+        {
+            if (IsEmpty)
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.LigaSiguiente = PrimerNodo;
+
+                PrimerNodo.LigaAnterior = nuevoNodo;
+
+                PrimerNodo = nuevoNodo;
+            }
+
+            NodoActual = nuevoNodo;
+
+            return "Nodo agregado al inicio...";
+        }
+        public string AgregarNodoAlFinal(Nodo nuevoNodo)
+        {
+            if (IsEmpty)
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.LigaAnterior = UltimoNodo;
+
+                UltimoNodo.LigaSiguiente = nuevoNodo;
+
+                UltimoNodo = nuevoNodo;
+            }
+
+            NodoActual = nuevoNodo;
+
+            return "Nodo agregado al Final...";
+        }
+
+        public Nodo Siguiente()
+        {
+            NodoActual = NodoActual.LigaSiguiente ?? UltimoNodo;
+            return NodoActual;
+        }
+
+        public Nodo Anterior()
+        {
+            NodoActual = NodoActual.LigaAnterior ?? PrimerNodo;
+            return NodoActual;
+        }
+
+
+    }
+}
